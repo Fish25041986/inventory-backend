87e5fe082140da3772368798dac26aa988d51eca
Fish25041986 <oscar-aguirre@hotmail.es>
2024-01-30 14:47:56 -0500

    septimo commit

--- src/main/java/com/complemento/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/complemento/inventory/services/CategoryServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.boot.Metadata;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
